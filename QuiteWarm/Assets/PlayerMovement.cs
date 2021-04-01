ff1b068dff9df2fdf5d3ad362779280ec4d25735
unknown <45499718+sqbly@users.noreply.github.com>
2021-04-01 17:20:49 +0200

    Bullets + time mechanic

--- QuiteWarm/Assets/PlayerMovement.cs
+++ QuiteWarm/Assets/PlayerMovement.cs
@@ -6,17 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
+    public Camera cam;
+
     private Vector2 moveDirection;
+    private Vector2 mousePos;
+
+    private float slowMo = 0.1f;
+    
 
     // Update is called once per frame
     void Update()
     {
         ProcessInputs();
+        AdjustTime();
     }
 
     void FixedUpdate() {
         //fiza
         Move();
+        Rotate();
     }
 
     void ProcessInputs() {
@@ -24,9 +32,24 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void AdjustTime() {
+        float scale = rb.velocity.magnitude / moveSpeed;
+
+        Time.timeScale = Mathf.Max(scale, slowMo);
+        Time.fixedDeltaTime = 0.005f * Time.timeScale;
     }
 
     void Move() {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void Rotate() {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
