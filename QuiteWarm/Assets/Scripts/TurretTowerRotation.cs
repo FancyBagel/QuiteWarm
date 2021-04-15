4a3444fa41996bca01ad1abefa2fa49e53e26696
Igosh <igosh509@gmail.com>
2021-04-15 16:10:52 +0200

    added Turret (w/o shooting), ordered files

---
+++ QuiteWarm/Assets/Scripts/TurretTowerRotation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretTowerRotation : MonoBehaviour
+{
+
+    public Rigidbody2D self;
+    public Rigidbody2D target;
+
+    void Update()
+    {
+        Vector2 lookDir = target.position - self.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        self.rotation = angle;
+        
+    }
+}
